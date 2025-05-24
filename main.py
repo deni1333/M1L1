4ede2762721a32b49690ede4483638563cb743cb
deni1333 <denchik_dzhabrailov@mail.ru>
2025-05-24 15:03:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-None
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input(" (большими буквами!): ")
-if word in meme_dict.keys():
-    # Что делать, если слово нашлось?
-else:
-    # Что делать, если слово не нашлось?
+import telebot
+    
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("ТУТ СЕКРЕТНЫЙ ТОКЕН")
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+# Запуск бота
+bot.polling()
